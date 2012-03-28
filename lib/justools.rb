4e6311273f34864f1ee19bebfc9bec8a5824b6c9
colin chun <caleon@gmail.com>
2012-03-28 00:18:00 -0700

    mistaken regexp

--- lib/justools.rb
+++ lib/justools.rb
@@ -2,6 +2,6 @@ require 'justools/core_ext'
 
 module Justools
   ARGS_AND_OPTS_REGEXP = /^#{%w(arg(?:ument)?s opt(?:ion)?s).
-                             zip(Array.new(2, "(?:_with_(\w+))?")).
+                             zip(Array.new(2, "(?:_with_(\\w+))?")).
                              map(&:join) * '_and_'}!$/
 end
